60545e7f0630e4778981e390efebb8e56e7f01e4
anandhu013 <anandhakrishnan013@gmail.com>
2023-08-01 18:29:15 +0530

    Adding ts files

---
+++ app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const server=express();
+server.get('/*',(req,res) => {
+    console.log(req.url);
+    res.status(200).send("Hello world typescript");
+})
+
+server.listen(3000,()=> {
+    console.log("Server is listening to 3000");
+});
